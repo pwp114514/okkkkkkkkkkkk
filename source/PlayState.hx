b3b2ae65bfb96ba524bc207fdc04c3c9ed64c6aa
pwp114514 <117086223+pwp114514@users.noreply.github.com>
2023-01-30 16:38:53 +0800

    jjjjj

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1554,38 +1554,6 @@ class PlayState extends MusicBeatState
 		char.y += char.positionArray[1];
 	}
 
-	public function startVideo(name:String)
-	{
-		#if VIDEOS_ALLOWED
-		inCutscene = true;
-
-		var filepath:String = Paths.video(name);
-		#if sys
-		if(!FileSystem.exists(filepath))
-		#else
-		if(!OpenFlAssets.exists(filepath))
-		#end
-		{
-			FlxG.log.warn('Couldnt find video file: ' + name);
-			startAndEnd();
-			return;
-		}
-		#if VIDEOS_ALLOWED 		
-		var video:VideoHandler = new VideoHandler();
-		video.playVideo(filepath);
-		video.finishCallback = function()
-		{
-			startAndEnd();
-			return;
-		}
-		#else
-		FlxG.log.warn('Platform not supported!');
-		startAndEnd();
-		return;
-		#end
-		#end
-	}
-
 	function startAndEnd()
 	{
 		if(endingSong)
